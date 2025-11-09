208d1b6b90a5fa6ef6aab1bf6ca381d769a1b35e
xiung <steven19790906@gmail.com>
2025-11-09 08:49:36 +0800

    feat: update cycling records for demo in HistoryView

--- frontend/src/views/HistoryView.vue
+++ frontend/src/views/HistoryView.vue
@@ -61,43 +61,169 @@ import MapView from '../components/MapView.vue'
 const novemberRecords = ref([
     {
         date: '2025/11/08',
-        time: '09:41',
-        duration: '00:09:41',
-        distance: '10.32 KM'
+        time: '18:23',
+        duration: '00:28:15',
+        distance: '7.82 KM'
     },
     {
-        date: '2025/11/08',
-        time: '09:41',
-        duration: '00:09:41',
-        distance: '10.32 KM'
+        date: '2025/11/07',
+        time: '07:45',
+        duration: '00:35:42',
+        distance: '9.15 KM'
     },
     {
-        date: '2025/11/08',
-        time: '09:41',
-        duration: '00:09:41',
-        distance: '10.32 KM'
+        date: '2025/11/06',
+        time: '19:10',
+        duration: '00:22:30',
+        distance: '5.64 KM'
+    },
+    {
+        date: '2025/11/04',
+        time: '08:20',
+        duration: '00:41:18',
+        distance: '11.23 KM'
+    },
+    {
+        date: '2025/11/03',
+        time: '17:55',
+        duration: '00:19:47',
+        distance: '4.92 KM'
+    },
+    {
+        date: '2025/11/01',
+        time: '14:30',
+        duration: '00:31:25',
+        distance: '8.47 KM'
     }
 ])
 
 // 10 月的騎行記錄
 const octoberRecords = ref([
     {
-        date: '2025/11/08',
-        time: '09:41',
-        duration: '00:09:41',
-        distance: '10.32 KM'
+        date: '2025/10/31',
+        time: '09:15',
+        duration: '00:44:20',
+        distance: '12.35 KM'
     },
     {
-        date: '2025/11/08',
-        time: '09:41',
-        duration: '00:09:41',
-        distance: '10.32 KM'
+        date: '2025/10/29',
+        time: '18:40',
+        duration: '00:26:53',
+        distance: '6.78 KM'
     },
     {
-        date: '2025/11/08',
-        time: '09:41',
-        duration: '00:09:41',
-        distance: '10.32 KM'
+        date: '2025/10/28',
+        time: '07:30',
+        duration: '00:38:12',
+        distance: '10.24 KM'
+    },
+    {
+        date: '2025/10/27',
+        time: '19:20',
+        duration: '00:21:45',
+        distance: '5.31 KM'
+    },
+    {
+        date: '2025/10/25',
+        time: '08:45',
+        duration: '00:33:28',
+        distance: '8.96 KM'
+    },
+    {
+        date: '2025/10/24',
+        time: '17:15',
+        duration: '00:29:37',
+        distance: '7.52 KM'
+    },
+    {
+        date: '2025/10/22',
+        time: '10:30',
+        duration: '00:46:15',
+        distance: '13.18 KM'
+    },
+    {
+        date: '2025/10/21',
+        time: '07:50',
+        duration: '00:24:42',
+        distance: '6.15 KM'
+    },
+    {
+        date: '2025/10/19',
+        time: '18:25',
+        duration: '00:36:50',
+        distance: '9.87 KM'
+    },
+    {
+        date: '2025/10/18',
+        time: '08:10',
+        duration: '00:27:18',
+        distance: '7.04 KM'
+    },
+    {
+        date: '2025/10/16',
+        time: '19:45',
+        duration: '00:40:33',
+        distance: '11.42 KM'
+    },
+    {
+        date: '2025/10/15',
+        time: '09:20',
+        duration: '00:23:55',
+        distance: '5.83 KM'
+    },
+    {
+        date: '2025/10/13',
+        time: '17:30',
+        duration: '00:34:47',
+        distance: '9.26 KM'
+    },
+    {
+        date: '2025/10/12',
+        time: '08:55',
+        duration: '00:28:22',
+        distance: '7.38 KM'
+    },
+    {
+        date: '2025/10/10',
+        time: '18:15',
+        duration: '00:42:10',
+        distance: '12.07 KM'
+    },
+    {
+        date: '2025/10/09',
+        time: '07:40',
+        duration: '00:25:48',
+        distance: '6.42 KM'
+    },
+    {
+        date: '2025/10/07',
+        time: '19:50',
+        duration: '00:37:25',
+        distance: '10.15 KM'
+    },
+    {
+        date: '2025/10/06',
+        time: '09:05',
+        duration: '00:30:14',
+        distance: '8.21 KM'
+    },
+    {
+        date: '2025/10/04',
+        time: '17:45',
+        duration: '00:26:37',
+        distance: '6.89 KM'
+    },
+    {
+        date: '2025/10/03',
+        time: '08:25',
+        duration: '00:39:52',
+        distance: '11.56 KM'
+    },
+    {
+        date: '2025/10/01',
+        time: '18:00',
+        duration: '00:32:18',
+        distance: '8.73 KM'
     }
 ])
 </script>
